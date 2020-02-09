6a54acf85e2a3ad8c2ab04de56a2fba278df771a
Joao Paulo <j.paaulinho@gmail.com>
2020-02-09 01:38:19 -0300

    primeira aplicacao utilizando hooks + typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-const App = () => {
+interface UserProps {}
+
+interface User {
+  matricula: number;
+  nome: string,
+  id_curso: number
+}
+
+const App: React.FC<UserProps> = () => {    //Criando o componente com Hooks e definindo seus props.
+
+  const [user, setUsers] = React.useState<User[]>([]);    //Definindo seu State e definindo o objeto do tipo User.
+
+  const getUsers = async () => {    //Realizando a Request
+    let response = await fetch('http://cleanderson.ddns.net:8080/students');
+    let users = await response.json();
+    setUsers(users);
+  }
+
+  React.useEffect( () => { getUsers() }, [])  //ComponentDidMount do Hooks.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {user.map( (users) => {
+          return console.log('Name: ' + users.nome + ', Email: ' + users.matricula);
+        })}
+      </div>
+    </>
   );
 }
 
